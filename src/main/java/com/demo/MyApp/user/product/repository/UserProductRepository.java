bfe73e7ca52299c79707c7f0604dd600b4608cb0
ds-jeong <jdaseul527@gmail.com>
2024-09-04 19:24:36 +0900

    JDBC의존성 삭제

--- src/main/java/com/demo/MyApp/user/product/repository/UserProductRepository.java
+++ src/main/java/com/demo/MyApp/user/product/repository/UserProductRepository.java
@@ -1,7 +1,6 @@
 package com.demo.MyApp.user.product.repository;
 
 import com.demo.MyApp.admin.product.entity.Product;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
