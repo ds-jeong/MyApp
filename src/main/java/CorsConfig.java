65f0a651c18c788f4aaeefc12738fa12ae003956
ds-jeong <jdaseul527@gmail.com>
2024-09-04 21:50:43 +0900

    Cors 처리2

--- src/main/java/CorsConfig.java
+++ src/main/java/CorsConfig.java
@@ -12,6 +12,6 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type", "Origin") // 클라이언트 측의 CORS 요청에 허용되는 헤더를 지정
                 .exposedHeaders("Custom-Header") // 클라이언트측 응답에서 노출되는 헤더를 지정
-                .allowCredentials(true) // 클라이언트 측에 대한 응답에 credentials(예: 쿠키, 인증 헤더)를 포함할 수 있는지 여부를 지정
+                .allowCredentials(true); // 클라이언트 측에 대한 응답에 credentials(예: 쿠키, 인증 헤더)를 포함할 수 있는지 여부를 지정
     }
 }
